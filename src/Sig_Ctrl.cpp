4aa2d9a5f26e46f476570cd0194ad07ffef79f27
Matthew Glazer <msg1254@rit.edu>
2017-05-11 00:28:50 -0400

    added some stuff

--- src/Sig_Ctrl.cpp
+++ src/Sig_Ctrl.cpp
@@ -1,11 +1,17 @@
 #include <Arduino.h>
 #include <Button.h>
-
+//--------------------------Button Pins-------------------------------
 #define BTN1 1
 #define BTN2 2
 #define BTN3 3
 #define BTN4 4 //change these values
 
+//--------------------------Button Setup-------------------------------
+#define PULLUP true
+#define INVERT true
+#define DEBOUNCE_MS 20
+
+//--------------------------Relay Pins-------------------------------
 #define RELAY_1 5
 #define RELAY_2 6
 #define RELAY_3 7
@@ -13,26 +19,30 @@
 #define RELAY_5 9
 #define RELAY_6 10 //change these values
 
-#define PULLUP true
-#define INVERT true
+//--------------------------Bulb Status-------------------------------
 #define SOLID  1
 #define FLASH  2
-#define RED    1
-#define YELLOW 2
-#define GREEN  3
-
-#define DEBOUNCE_MS 20
 
 int bulb1_state = SOLID;
 int bulb2_state = SOLID;
 
+//--------------------------Bulb Color-------------------------------
+#define RED    1
+#define YELLOW 2
+#define GREEN  3
+
 int bulb1_color = RED;
 int bulb2_color = RED;
+
 
 Button toggle1(BTN1, PULLUP, INVERT, DEBOUNCE_MS);
 Button toggle2(BTN2, PULLUP, INVERT, DEBOUNCE_MS);
 Button toggle3(BTN3, PULLUP, INVERT, DEBOUNCE_MS);
 Button toggle4(BTN4, PULLUP, INVERT, DEBOUNCE_MS);
+
+//--------------------------Functions-------------------------------
+void change_flashing();
+void change_color();
 
 void setup(void)
 {
@@ -50,6 +60,9 @@ void loop(void)
   toggle2.read();
   toggle3.read();
   toggle4.read();
+
+  change_flashing();
+  change_color();
 
 
 }
@@ -97,7 +110,51 @@ void change_flashing()
 
 void change_color()
 {
+  int b1 = 0;
+  int b2 = 0;
 
+  if (toggle3.wasReleased())
+  {
+    b1++;
+
+    if (b1 == 1)
+    {
+      bulb1_color = RED;
+    }
+    if (b1 == 2)
+    {
+      bulb1_color = YELLOW;
+    }
+    if (b1 == 3)
+    {
+      bulb1_color = GREEN;
+    }
+    else
+    {
+      b1 = 0;
+    }
+  }
+  if (toggle4.wasReleased())
+  {
+    b2++;
+
+    if (b2 == 1)
+    {
+      bulb2_color = RED;
+    }
+    if (b2 == 2)
+    {
+      bulb2_color = YELLOW;
+    }
+    if (b1 == 3)
+    {
+      bulb2_color = GREEN;
+    }
+    else
+    {
+      b2 = 0;
+    }
+  }
 }
 
 void primary_relay_ctrl()
